948510847b56ea4cd7785e6765136dadc085c04e
MiotaczWymiatacz <tigerwait97@gmail.com>
2020-09-23 20:06:49 +0200

    Updates

---
+++ precache-manifest.9fbb957677e9d6717131a7cd8a7b306e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ff0c10beb7a3529775acd477ef0a4c",
+    "url": "/HigherTask/index.html"
+  },
+  {
+    "revision": "b9d50add791c19e163d1",
+    "url": "/HigherTask/static/css/main.dc48d941.chunk.css"
+  },
+  {
+    "revision": "604254747f2b977b4ef1",
+    "url": "/HigherTask/static/js/2.e58646b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HigherTask/static/js/2.e58646b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d50add791c19e163d1",
+    "url": "/HigherTask/static/js/main.d5d9b4f4.chunk.js"
+  },
+  {
+    "revision": "038e9e57e1673142f586",
+    "url": "/HigherTask/static/js/runtime-main.d47af662.js"
+  }
+]);
